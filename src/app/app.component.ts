64bc2a476ca3acdd9bc4b16bbfd47146e685c31c
TM <adolf.hazmi@yahoo.com>
2024-05-13 00:31:54 +0000

    Display events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {BossComponent} from "./boss/boss.component";
 import {EventComponent} from "./event/event.component";
+import {ServertimeComponent} from "./servertime/servertime.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BossComponent, EventComponent],
+  imports: [RouterOutlet, BossComponent, EventComponent, ServertimeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,30 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {OnInit} from '@angular/core';
 import {EventService} from "./event.service";
 import {EVENT} from "./event";
 import {BossComponent} from "../boss/boss.component";
 import {ServertimeComponent} from "../servertime/servertime.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-event',
   standalone: true,
   imports: [
+    CommonModule,
     BossComponent,
     ServertimeComponent
   ],
-  providers:[EventService],
+  providers: [EventService],
   templateUrl: './event.component.html',
   styleUrl: './event.component.css'
 })
-export class EventComponent implements OnInit{
-  events :EVENT[] | undefined;
+export class EventComponent implements OnInit {
+  events: EVENT[] | undefined;
 
   constructor(private eventService: EventService) {
   }
 
-
-  ngOnInit(){
-    this.eventService.getEvents().subscribe(data =>
-    this.events=data)
+  ngOnInit() {
+    this.eventService.getEvents().subscribe(data => {
+      this.events = data
+    })
   }
 }
