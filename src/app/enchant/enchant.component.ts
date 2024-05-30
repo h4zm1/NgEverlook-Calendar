f1fc300c3ba3949f802e651a2c596e00042429a9
h4zm1 <adolf.hazmi@yahoo.com>
2024-05-30 23:55:20 +0100

    Fix enchant page ui

--- src/app/enchant/enchant.component.ts
+++ src/app/enchant/enchant.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import {ThemeService} from "../theme.service";
 
 @Component({
   selector: 'app-enchant',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './enchant.component.html',
   styleUrl: './enchant.component.scss'
 })
-export class EnchantComponent {
-
+export class EnchantComponent implements OnInit{
+  themeService: ThemeService = inject(ThemeService);
+  ngOnInit(){
+    // if no local storage theme var available, make it light and save it
+    if (localStorage.getItem('theme') == null) {
+      this.themeService.setTheme("light")
+      localStorage.setItem('theme', 'light');
+    }
+    // if local var is dark, open the curtain and set theme to dark
+    if (localStorage.getItem("theme") == "dark") {
+      this.themeService.setTheme("dark")
+    }
+  }
 }
