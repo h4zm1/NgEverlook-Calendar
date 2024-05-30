8285ef4a9b3343c1549fe517db744a7e464e1c89
h4zm1 <adolf.hazmi@yahoo.com>
2024-05-30 22:26:47 +0100

    Add theme state to local storage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,11 +28,21 @@ import {CommonModule} from '@angular/common';
 export class HomeComponent implements OnInit {
   title = 'NgEverlook-Calendar';
   themeService: ThemeService = inject(ThemeService);
-  activeTheme: string = "light"
-  state = 0;
+  // activeTheme: string = "light"
+  state = 0; // 1 = dark, 0 = light
 
   ngOnInit() {
-    this.themeService.setTheme(this.activeTheme)
+    // if no local storage theme var available, make it light and save it
+    if (localStorage.getItem('theme') == null) {
+      this.themeService.setTheme("light")
+      localStorage.setItem('theme', 'light');
+    }
+    console.log(localStorage.getItem("theme"));
+    // if local var is dark, open the curtain and set theme to dark
+    if (localStorage.getItem("theme") == "dark") {
+      this.state = 1;
+      this.themeService.setTheme("dark")
+    }
   }
 
 
@@ -41,8 +51,10 @@ export class HomeComponent implements OnInit {
     // and only wait 100ms when the curtain is turning off
     setTimeout(() => {
       this.themeService.updateTheme()
+      localStorage.setItem("theme", this.themeService.themeSignal().toString());
     }, this.themeService.themeSignal() === "light" ? 500 : 100)
     // to trigger the active state on the curtain html element
     this.state = this.state === 1 ? 0 : 1;
+
   }
 }
