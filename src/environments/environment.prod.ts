5f23c1d96a4137573d979d7865ebea3bc781b94b
h4zm1 <adolf.hazmi@yahoo.com>
2025-12-04 18:53:19 +0100

    Fix: change apiurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   port: 0, // Production port (usually not needed)
-  apiUrl: 'https://everwenback.fr.to',
+  apiUrl: 'https://api.hazmimosbah.com',
   // apiUrl: 'http://localhost:8080',
   enableLogging: false,
 };
